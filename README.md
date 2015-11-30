eddd56d87af8befa1daf3d62d14a5008fd2d6fe4
CC Lien <cclien@gmail.com>
2015-12-01 02:06:51 +0800

    NaviKing DB export

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GoStation-POI-Export
 GoStation 站點匯出工具
+
+### 匯出導航王自建點資料庫
+
+Step1. 準備一份能動的 favorites.db
+
+Step2. 執行 script 匯出站點資訊到 DB (注意：會把全部自建點清空)
+
+`$ python gostation_navking.py favorites.db`
---
+++ gostation_navking.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+
+    Creating NaviKing's favorites.db from GoStations list API
+    WARNING: This script overrides all your existing POIs in the database
+
+"""
+import urllib, json
+import sqlite3
+import time
+import sys
+
+TABLE_NAME = "favListV0"
+DROP_TABLE_STMT = '''DROP TABLE IF EXISTS favListV0 '''
+CREATE_TABLE_STMT = '''CREATE TABLE favListV0 (_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                     poi_name TEXT,poi_image_id INTEGER,poi_image_name TEXT,
+                     poi_photo_path TEXT,category_name TEXT,phone_number TEXT,
+                     location TEXT,lat REAL,lon REAL,buffer_string TEXT,
+                     buffer_index INTEGER,basic_option INTEGER,extra_option INTEGER,
+                     region TEXT,create_time LONG,use_frequency INTEGER,CRoad INTEGER)'''
+TEST_DB_STMT = "SELECT * FROM android_metadata"
+LANG = "zh-TW"
+QUOTE_SIGN = "\""
+COMMON_FIELDS = {"create_time":int(time.time()), "poi_image_id":-1, "poi_image_name":"",\
+                 "poi_photo_path":"", "category_name":"", "phone_number":"", "location":"",\
+                 "buffer_string":"", "buffer_index":-1, "basic_option":13, "extra_option":0,\
+                 "use_frequency":0, "CRoad":0}
+URL = "http://www.gogoro.com/tw/api/vm/list"
+
+def get_lang(list_jsonstring):
+    """ The API return multilanguages fields with following format:
+        "City":"{\"List\":[{\"Value\":\"Taipei City\",\"Lang\":\"en-US\"},
+                           {\"Value\":\"台北市\",\"Lang\":\"zh-TW\"}]}"
+        Just grab the specified language we want """
+    gogoro_list = json.loads(list_jsonstring)
+    for item in gogoro_list['List']:
+        if item['Lang'] == LANG:
+            return item['Value']
+
+    raise Exception("language %s not found" % LANG)
+
+def prepare_row(data):
+    """ Prepare DB row from API data """
+    output = {}
+    output['poi_name'] = get_lang(data['LocName'])
+    output['lat'] = data['Latitude']
+    output['lon'] = data['Longitude']
+    output['region'] = "%s,%s" % (get_lang(data['City']), get_lang(data['District']))
+
+    return output
+
+def insert_sql(cur, data):
+    """ insert the dict into the SQLite DB """
+    columns = ','.join(data.keys())
+    placeholders = ', '.join('?' * len(data))
+    sql = 'INSERT INTO {} ({}) VALUES ({})'.format(TABLE_NAME, columns, placeholders)
+    cur.execute(sql, data.values())
+
+def main(argv):
+    """ just main function here """
+
+    if len(argv) < 2:
+        print >> sys.stderr, "usage: %s [sqlite db filename]" % argv[0]
+        return
+
+    filename = argv[1]
+
+    response = urllib.urlopen(URL)
+    jsonbody = json.loads(response.read().decode('utf-8'))
+
+    conn = sqlite3.connect(filename)
+    cur = conn.cursor()
+
+    try:
+        cur.execute(TEST_DB_STMT)
+    except sqlite3.OperationalError:
+        print >> sys.stderr, "%s seems not like a working NavKing POI DB" % filename
+        return
+
+    cur.execute(DROP_TABLE_STMT)
+    cur.execute(CREATE_TABLE_STMT)
+
+    for data in jsonbody['data']:
+        row = prepare_row(data)
+        row.update(COMMON_FIELDS)
+        insert_sql(cur, row)
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
